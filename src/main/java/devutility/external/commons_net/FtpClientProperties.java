e61b2ad938270159df3c823f47cc30c0c9dac582
Aldwin Su <fangyansu@163.com>
2019-07-20 23:56:37 +0800

    Update FtpClientProperties

--- src/main/java/devutility/external/commons_net/FtpClientProperties.java
+++ src/main/java/devutility/external/commons_net/FtpClientProperties.java
@@ -10,13 +10,60 @@ import devutility.internal.net.UrlUtils;
  * @version: 2019-07-17 19:39:38
  */
 public class FtpClientProperties {
+	/**
+	 * ftp or sftp.
+	 */
 	private String protocol;
+
+	/**
+	 * Host address for Ftp server.
+	 */
 	private String host;
+
+	/**
+	 * Port number for Ftp server.
+	 */
 	private int port = 21;
+
+	/**
+	 * Login name for Ftp server.
+	 */
 	private String userName;
+
+	/**
+	 * Password for Ftp server.
+	 */
 	private String password;
+
+	/**
+	 * Whether Ftp server should enable passive mode or not?
+	 */
 	private boolean passiveMode;
+
+	/**
+	 * Encoding type for Ftp transmission.
+	 */
 	private String encoding = "UTF-8";
+
+	/**
+	 * Ftp connection timeout in milliseconds.
+	 */
+	private int connectTimeout;
+
+	/**
+	 * Internal buffer size for buffered data streams.
+	 */
+	private int bufferSize = 1024;
+
+	/**
+	 * Sets the file type to be transferred. This should be one of <code>FTP.ASCII_FILE_TYPE</code>,
+	 * <code>FTP.BINARY_FILE_TYPE</code>, etc. The default file type is <code>FTP.ASCII_FILE_TYPE</code>.
+	 */
+	private int fileType;
+
+	/**
+	 * Start path of Ftp server catalog.
+	 */
 	private String start;
 
 	public String getProtocol() {
@@ -73,6 +120,30 @@ public class FtpClientProperties {
 
 	public void setEncoding(String encoding) {
 		this.encoding = encoding;
+	}
+
+	public int getConnectTimeout() {
+		return connectTimeout;
+	}
+
+	public void setConnectTimeout(int connectTimeout) {
+		this.connectTimeout = connectTimeout;
+	}
+
+	public int getBufferSize() {
+		return bufferSize;
+	}
+
+	public void setBufferSize(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+	public int getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(int fileType) {
+		this.fileType = fileType;
 	}
 
 	public String getStart() {
